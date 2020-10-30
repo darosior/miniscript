74e210d0256e178d6d32fb0bb3b51a92807e6570
sanket1729 <sanket1729@gmail.com>
2020-10-30 07:59:27 -0500

    Update IsSafe() to consider TimelockInfo
    The top level miniscript is considered safe if
    the the miniscript represents the underlying
    semantics obtained after lifting.
    In case, there is atleast one possible satisfaction
    path that could require height-based and time-based
    satisfactions, we consider that as unsafe and the
    miniscript properties are no longer guaranteed

--- bitcoin/script/miniscript.h
+++ bitcoin/script/miniscript.h
@@ -764,6 +764,9 @@ public:
     //! Check the maximum stack size for this script against the policy limit.
     bool CheckStackSize() const { return GetStackSize() <= MAX_STANDARD_P2WSH_STACK_ITEMS; }
 
+    //! Check whether there is no satisfaction path that contains both timelocks and heightlocks
+    bool CheckTimeLocksMix() const { return GetType() << "k"_mst; }
+
     //! Return the expression type.
     Type GetType() const { return typ; }
 
@@ -780,7 +783,7 @@ public:
     bool NeedsSignature() const { return GetType() << "s"_mst; }
 
     //! Do all sanity checks.
-    bool IsSafeTopLevel() const { return GetType() << "Bms"_mst && CheckOpsLimit() && CheckStackSize(); }
+    bool IsSafeTopLevel() const { return GetType() << "Bms"_mst && CheckOpsLimit() && CheckStackSize() && CheckTimeLocksMix(); }
 
     //! Construct the script for this miniscript (including subexpressions).
     template<typename Ctx>
